cf264122f96ecb89eb8eebeceaf31c5e0841eab9
ryllerfeitosa <you@ryllerfeitosa@gmail.com>
2026-05-04 08:19:27 -0300

    Projeto finalizado

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const criarCard = function(listaProdutos){
     card.className = 'card'
 
     //Categoria do produto
-    const categoria       = document.createElement('categoria')
+    const categoria       = document.createElement('p')
     categoria.textContent = listaProdutos.categoria
 
     //Imagem referente ao produto
     const foto = document.createElement('img')
     foto.src   = `./img/${listaProdutos.imagem}`
-    foto.alt   = `./img/${listaProdutos.nome}`
+    foto.alt   = `Foto de ${listaProdutos.nome}`
     
     //Nome do produto
     const nome = document.createElement('h2')
@@ -22,14 +22,31 @@ const criarCard = function(listaProdutos){
     const descricao = document.createElement('details')
     descricao.textContent = listaProdutos.descricao
 
+    //Classificação do produto
+    const classificacao = document.createElement('img')
+    if(Number(listaProdutos.classificacao) == 2){
+        classificacao.src = `./img/2 estrela.png`
+    }else if(Number(listaProdutos.classificacao) == 3){
+        classificacao.src = `./img/3 estrela.png`
+    }else if(Number(listaProdutos.classificacao) == 4){
+        classificacao.src = `./img/4 estrela.png`
+    }else if(Number(listaProdutos.classificacao) == 5){
+        classificacao.src = `./img/5 estrela.png`
+    }else{
+
+    }
+
     //Preço do produto
     const preco = document.createElement('h3')
     preco.textContent = listaProdutos.preco
 
-    card.append(categoria, foto, nome, descricao)
+    card.append(categoria, foto, nome, descricao, preco, classificacao)
     return card
 }
 
-const card = listaProdutos.map(criarCard)
+//Percorrer o array de listaProdutos antes de fazer a função map, pois o retorno de listaProdutos é um array de objetos
+const produtos = listaProdutos()
+
+const card = produtos.map(criarCard)
 //Substituir os elementos presentes por esses filhos
 document.getElementById('container').replaceChildren(...card)
